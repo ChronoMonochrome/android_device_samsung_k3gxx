69341ccb5b66d0d4e2079a2c3b920aa4307fdf8b
quanganh2627 <quanganh2627@gmail.com>
2016-06-20 18:01:36 +0700

    Add support build userdebug & user variant

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,6 @@
 add_lunch_combo cm_k3gxx-eng
+add_lunch_combo cm_k3gxx-userdebug
+add_lunch_combo cm_k3gxx-user
 
 mkdir -p out/cm12/target/product/trelte/obj/KERNEL_OBJ/drivers/sensorhub/stm/factory
 mkdir -p out/cm12/target/product/trelte/obj/KERNEL_OBJ/drivers/media/platform/exynos/fimc-is/sensor
